37edd3a175fb22357baf07cc685dda44dca311ad
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-07-17 18:49:47 +0530

    update for swaync

--- swaync/.config/swaync/scripts/toggle-center.sh
+++ swaync/.config/swaync/scripts/toggle-center.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
-swaync-client --toggle-panel
-
+# Toggle center and clear all if already open
+if pgrep -x swaync >/dev/null; then
+  swaync-client -t
+  sleep 0.2
+  swaync-client -c
+else
+  swaync-client -t
+fi
