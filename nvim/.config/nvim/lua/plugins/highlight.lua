02f30a3d19b8ea9d876442b2d97c6a012327c2fa
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-07-27 18:14:57 +0530

    log

--- nvim/.config/nvim/lua/plugins/highlight.lua
+++ nvim/.config/nvim/lua/plugins/highlight.lua
@@ -3,7 +3,7 @@ return {
   event = "BufReadPre",
   config = function()
     require("nvim-highlight-colors").setup({
-      render = "virtual", -- or 'virtual', or 'foreground', or 'background'
+      render = "background", -- or 'virtual', or 'foreground', or 'background'
       enable_named_colors = false,
       enable_tailwind = true,
     })
